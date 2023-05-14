59a2d1c421da1a801307cbcddeb936a0ef392b60
rauch <rauchrodrigues@gmail.com>
2023-05-14 13:59:03 +0530

    1st push

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [gender, setGender] = useState("shirt");
+
+  function updatefunc(e) {
+    let gender = e.target.getAttribute("id");
+
+    if (gender === "male") {
+      setGender("shirt");
+    }
+    else {
+      setGender("dress");
+    }
+  }
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Select your gender:</h1>
+      <label htmlFor="male">Male</label>
+      <input type="radio" name="gender" id="male" onChange={updatefunc} />
+      <label htmlFor="female">Female</label>
+      <input type="radio" name="gender" id="female" onChange={updatefunc} />
+
+      <h1>Select your {gender} size</h1>
+      <select name="size">
+        <option value="2">2</option>
+        <option value="4">4</option>
+        <option value="6">6</option>
+      </select>
+
     </div>
   )
 }
